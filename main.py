f6dbe116f18a3c2a95a5192e6936762da0243e5f
Qobiljon Qosimov <59614378+cody0313@users.noreply.github.com>
2022-10-18 21:31:44 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import cv2
+from deepface import DeepFace
+
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+
+cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    cam = cv2.VideoCapture(1)
+if not cam.isOpened():
+    raise IOError("Webcam error")
+
+
+while True:
+    ret,frame = cam.read()
+    result = DeepFace.analyze(frame, actions = ["emotion"])
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+
+    for(x, y, w, h ) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + x), (0, 255, 0), 2)
+
+    font = cv2.FONT_HERSHEY_DUPLEX
+
+
+    #cv2.putText(frame,
+    #           result['gender'],
+    #           (100, 50),
+    #           font, 1,
+    #           (220, 0, 0),
+    #           1,
+    #           cv2.LINE_4)
+    #cv2.putText(frame,
+    #           result['age'],
+    #           (40, 50),
+    #           font, 1,
+    #           (220, 0, 0),
+    #           2,
+    #           cv2.LINE_4)
+    #cv2.putText(frame,
+    #           result['dominant_race'],
+    #           (100, 100),
+    #           font, 1,
+    #           (220, 0, 0),
+    #           1,
+    #           cv2.LINE_4)
+    cv2.putText(frame,
+               result['dominant_emotion'],
+               (100, 150),
+               font, 1,
+               (220, 0, 0),
+               1,
+               cv2.LINE_4)
+
+    
+                
+    cv2.imshow("Demo Video", frame)
+
+    if  cv2.waitKey(2) & 0xFF == ord("e"):
+        break
+
+
+cam.release()
+cv2.destroyAllWindows()
